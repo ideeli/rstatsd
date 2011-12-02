8e25b5c7ea706ce7ac067526e20fca8d98d3a455
Aaron Brown <aaron@9minutesnooze.com>
2011-12-02 15:39:13 -0500

    daemonizing (still with some debug code)

--- rstatsd.rb
+++ rstatsd.rb
@@ -10,6 +10,7 @@ require 'rubygems'
 require 'statsd'
 require 'logger'
 
+
 # this program needs named capture groups, which are not available
 # in Ruby 1.8.x
 if RUBY_VERSION =~ /1\.8/
@@ -36,6 +37,107 @@ def setup_logger ( logfile, loglevel = "INFO"  )
   logger
 end
 
+class ProcessCtl
+  STARTCMD, STOPCMD, STATUSCMD = "start","stop","status" 
+
+  attr_accessor :pidfile, :daemonize
+
+  def initialize
+    @pidfile = ""
+    @daemonize = false
+    @pid = nil
+  end
+
+  def start 
+    trap(:INT)     { cleanup; stop }
+    trap(:SIGTERM) { cleanup; stop }
+
+    size = get_running_pids.size
+    if size > 0
+      puts "Daemon is already running"
+      return 1
+    end
+
+#    Daemonize.daemonize if @daemonize
+    if @daemonize
+      #http://stackoverflow.com/questions/1740308/create-a-daemon-with-double-fork-in-ruby
+      raise 'First fork failed' if (pid = fork) == -1
+      exit unless pid.nil?
+
+      Process.setsid
+      raise 'Second fork failed' if (pid = fork) == -1
+      exit unless pid.nil?
+
+      Dir.chdir '/'
+      File.umask 0000
+      STDIN.reopen '/dev/null'
+      STDOUT.reopen '/dev/null', 'a'
+      STDERR.reopen STDOUT
+    end
+    write_pid unless pidfile == ""
+    yield
+    return 0
+  end
+
+  def stop
+    # call user code if defined
+    begin 
+      yield 
+    rescue
+    end
+    get_running_pids.uniq.each do |pid|
+      puts "Killing pid #{pid}"
+      Process.kill("INT", pid)
+      # can't do anything below here.  Process is dead
+    end
+    return 0
+  end
+
+  # returns the exit status (1 if not running, 0 if running)
+  def status
+    size = get_running_pids.size
+    puts "#{File.basename $0} is #{"not " if size < 1}running."
+    return (size > 0) ? 0 : 1
+  end
+
+protected
+  def cleanup
+    File.delete(@pidfile) if File.file?(@pidfile) 
+#    exit 0
+  end
+
+  def write_pid
+    @pid = Process.pid
+    File.open(@pidfile, "w") do |f|
+#      f.write($$)
+      f.write(Process.pid)
+    end
+  end
+
+  def get_running_pids
+    return get_child_pids(pid) if @pid
+    result = []
+    if File.file? @pidfile
+      pid = File.read @pidfile
+      #result = `ps -p #{pid} -o pid | sed 1d`.to_a.map!{|x| x.to_i}
+      @allpids = `ps -ef |sed 1d`.to_a.map { |x| a = x.strip.split(/\s+/); [a[1].to_i,a[2].to_i] }
+      puts "getting children of #{pid}"
+      result = get_child_pids(pid.to_i) 
+      pp result
+    end
+    return result
+  end
+
+
+  def get_child_pids ( ppid )
+    child_pids = @allpids.select { |x| x[1] == ppid }.map { |x| x[0] }
+    pids = child_pids
+    child_pids.each do |pid|
+      pids += get_child_pids(pid)
+    end
+    pids
+  end
+end
 
 class Hash
   def prefix_keys ( prefix )
@@ -76,10 +178,10 @@ module RStatsd
       
       logit("Starting thread for command #{@command}")
       pipe = IO.popen(@command)
+      trap(:INT) { Process.kill(:INT, pipe.pid) }
 
-      pipe.each_with_index do |l,i|
-        trap("INT") { Process.kill("INT", pipe.pid) }
-        begin
+      begin
+        pipe.each_with_index do |l,i|
           @regexes.each { |r| h = r.get_increments(l, h) }
           # only send to statsd every x lines - this is to avoid UDP floods
           if (i % @every) == 0
@@ -87,13 +189,13 @@ module RStatsd
             statsd_send(h) 
             h = {}
           end
-        rescue Exception => e
-          logit(e, Logger::ERROR)
-          Process.kill("INT", pipe.pid) 
+          # this is for debugging
+          sleep(rand/100)
         end
-
-        # this is for debugging
-        sleep(rand/100)
+      rescue Exception => e
+        logit(e, Logger::ERROR)
+      ensure
+        Process.kill("INT", pipe.pid) 
       end
     end
 
@@ -155,7 +257,10 @@ module RStatsd
   end
 end
 
-options = { :cfg_file => File.dirname(__FILE__)+'/rstatsd.yaml' }
+options = { :cfg_file  => File.dirname(__FILE__)+'/rstatsd.yaml',
+            :pidfile   => '/tmp/rstatsd.pid',
+            :ctl_cmd   => ARGV[0],
+            :daemonize => false }
 
 cfg = YAML.load_file(options[:cfg_file])
 
@@ -177,5 +282,22 @@ cfg[:cmds].each do |cmd|
   end
 end
 
-commands.each { |c| threads << Thread.new { c.execute! }; sleep 1 }
-threads.each { |t| t.join }
+pc = ProcessCtl.new
+pc.daemonize = options[:daemonize]
+pc.pidfile   = options[:pidfile]
+
+case options[:ctl_cmd]
+when ProcessCtl::STOPCMD 
+  pc.stop { puts "I'm done" }
+when ProcessCtl::STATUSCMD 
+  exit pc.status
+else
+  code = pc.start do
+    commands.each do |c| 
+      thread = Thread.new { c.execute! }
+      threads << thread
+    end
+    threads.each { |t| t.join } ;
+  end
+  exit code
+end
