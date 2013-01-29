7563f256085fee2935c0f4967baa4b172269c835
Aaron Brown <aaron@9minutesnooze.com>
2013-01-29 10:49:03 -0500

    adding timers to rstatsd

--- lib/rstatsd/rstatsd.rb
+++ lib/rstatsd/rstatsd.rb
@@ -29,18 +29,27 @@ module RStatsd
     def execute!
       trap(:INT)  { logit("Caught SIGINT.  Exiting");  Process.kill(:INT,  pipe.pid) }
       trap(:TERM) { logit("Caught SIGTERM.  Exiting"); Process.kill(:TERM, pipe.pid) }
-      h = {}
+      
+      timers, counters = {}, {}
       
       logit("Starting thread for command #{@command}")
       pipe = IO.popen(@command)
       begin
         pipe.each_with_index do |l,i|
-          @regexes.each { |r| h = r.get_increments(l, h) }
+          @regexes.each do |r| 
+            if r.statsd_type == RegExData::Timer
+              timers = r.get_increments(l, timers)
+            else
+              counters = r.get_increments(l, counters)
+            end
+          end
+          
           # only send to statsd every x lines - this is to avoid UDP floods
           if (i % @every) == 0
             logit("#{i} lines, sending to statsd", Logger::DEBUG) 
-            statsd_send(h) 
-            h = {}
+            statsd_send(counters) 
+            statsd_send(divide_hash(timers, @every), 'timer')
+            timers, counters = {}, {}
           end
           # this is for debugging
 #sleep(rand/100)
@@ -52,12 +61,22 @@ module RStatsd
       end
     end
 
-    def statsd_send ( h )
+    # divide all values in Hash h by divisor
+    def divide_hash ( h, divisor )
+      Hash[h.map { |k,v| [k, v /= divisor.to_f] }]
+    end
+
+    def statsd_send ( h, statsd_type = nil )
       # prefix keys unless they are prefixed by '/'
       h.prefix_keys(@prefix) { |k,v| k[0,1] != '/' }.each do |k,v| 
         k = k.gsub(/^\//,'')  # strip leading '/'
-        Statsd.update_counter(k,v)
-        logit("Incremented #{k} by #{v}",Logger::DEBUG) 
+        if statsd_type == 'timer'
+          Statsd.timing(k,v)
+          logit("Set timer value #{k} to #{v}",Logger::DEBUG) 
+        else
+          Statsd.update_counter(k,v)
+          logit("Incremented #{k} by #{v}",Logger::DEBUG) 
+        end
       end
     end
   end
@@ -66,14 +85,26 @@ module RStatsd
     include Helpers
 
     attr_writer :logger
+    attr_reader :regex, :statsd_type
+
+    Counter, Timer = 1, 2
 
     def initialize ( h )
-      @regex     = which_regex h[:regex]
-      @metrics   = h[:metrics] || []
-      @statsd    = h[:statsd] || true
-      @carbon    = !!h[:carbon] 
-      @logger    = h[:logger]
-      @use_value = !!h[:use_value] 
+      @regex       = which_regex h[:regex]
+      @metrics     = h[:metrics] || []
+      @statsd_type = case h[:statsd_type]
+                     when 'timer' then Timer 
+                     else Counter 
+                     end
+      @statsd      = h[:statsd] || true
+      @carbon      = !!h[:carbon] 
+      @logger      = h[:logger]
+      @use_value   = !!h[:use_value] 
+
+      # timers implicitly use the value
+      if @statsd_type == Timer
+        @use_value = true
+      end
     end
 
     def get_increments ( line, h )
